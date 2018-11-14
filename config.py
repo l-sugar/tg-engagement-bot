8ea9e2546ffe334687c7ab4f8836744b6883af32
Louis Svugr <l.svugr@gmail.com>
2018-11-14 16:40:51 +0100

    change times to testing

--- config.py
+++ config.py
@@ -13,18 +13,18 @@ INSTA_USERNAME = os.getenv('INSTA_USER')
 INSTA_PASSWORD = os.getenv('INSTA_PW')
 
 # TIMES
-ROUNDS_INTERVAL = 23.5 * 60 * 60  # interval between rounds, seconds
-DROP_WINDOW = 30 * 60  # drop window before each round_start, seconds
-ROUND_TIME = 1 * 30 * 60  # round_start time, seconds
-DROP_ANNOUNCE = 1 * 60 * 60 # drop_announcement time, seconds
-DROP_ENDS_SOON = DROP_WINDOW * 5 // 6
+# ROUNDS_INTERVAL = 23.5 * 60 * 60  # interval between rounds, seconds
+# DROP_WINDOW = 30 * 60  # drop window before each round_start, seconds
+# ROUND_TIME = 1 * 30 * 60  # round_start time, seconds
+# DROP_ANNOUNCE = 1 * 60 * 60 # drop_announcement time, seconds
+# DROP_ENDS_SOON = DROP_WINDOW * 5 // 6
 
 # TESTING TIMES
-# ROUNDS_INTERVAL = 1 * 25 * 60  # interval between rounds, seconds TESTING
-# DROP_WINDOW = 10 * 60  # drop window before each round_start, seconds TESTING
-# ROUND_TIME = 1 * 20 * 60  # round_start time, seconds TESTING
-# DROP_ANNOUNCE = 10 * 60 # drop_announcement time, seconds TESTING
-# DROP_ENDS_SOON = DROP_WINDOW * 5 // 6
+ROUNDS_INTERVAL = 1 * 25 * 60  # interval between rounds, seconds TESTING
+DROP_WINDOW = 10 * 60  # drop window before each round_start, seconds TESTING
+ROUND_TIME = 1 * 20 * 60  # round_start time, seconds TESTING
+DROP_ANNOUNCE = 10 * 60 # drop_announcement time, seconds TESTING
+DROP_ENDS_SOON = DROP_WINDOW * 5 // 6
 
 BAD_USER_BAN_TIME = 15 * 24 * 60 * 60 # user ban time after bad behavior
 #BAD_USER_BAN_TIME = 1 * 60 * 60 # user ban time after bad behavior TESTING
