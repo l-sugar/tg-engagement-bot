e72957d435e1c55a9682028c6ff3c845b36f5964
Louis Svugr <l.svugr@gmail.com>
2018-11-16 23:07:54 +0100

    set to testing times

--- config.py
+++ config.py
@@ -13,21 +13,21 @@ INSTA_USERNAME = os.getenv('INSTA_USER')
 INSTA_PASSWORD = os.getenv('INSTA_PW')
 
 # TIMES
-ROUNDS_INTERVAL = 23.5 * 60 * 60  # interval between rounds, seconds
-DROP_WINDOW = 30 * 60  # drop window before each round_start, seconds
-ROUND_TIME = 1 * 30 * 60  # round_start time, seconds
-DROP_ANNOUNCE = 1 * 60 * 60 # drop_announcement time, seconds
-DROP_ENDS_SOON = DROP_WINDOW * 5 // 6
-
-# TESTING TIMES
-# ROUNDS_INTERVAL = 1 * 2 * 60  # interval between rounds, seconds TESTING
-# DROP_WINDOW = 1 * 60  # drop window before each round_start, seconds TESTING
-# ROUND_TIME = 1 * 0.1 * 60  # round_start time, seconds TESTING
-# DROP_ANNOUNCE = 0.5 * 60 # drop_announcement time, seconds TESTING
+# ROUNDS_INTERVAL = 23.5 * 60 * 60  # interval between rounds, seconds
+# DROP_WINDOW = 30 * 60  # drop window before each round_start, seconds
+# ROUND_TIME = 1 * 30 * 60  # round_start time, seconds
+# DROP_ANNOUNCE = 1 * 60 * 60 # drop_announcement time, seconds
 # DROP_ENDS_SOON = DROP_WINDOW * 5 // 6
 
-BAD_USER_BAN_TIME = 15 * 24 * 60 * 60 # user ban time after bad behavior
-#BAD_USER_BAN_TIME = 1 * 60 * 60 # user ban time after bad behavior TESTING
+# TESTING TIMES
+ROUNDS_INTERVAL = 1 * 2 * 60  # interval between rounds, seconds TESTING
+DROP_WINDOW = 1 * 60  # drop window before each round_start, seconds TESTING
+ROUND_TIME = 1 * 0.1 * 60  # round_start time, seconds TESTING
+DROP_ANNOUNCE = 0.5 * 60 # drop_announcement time, seconds TESTING
+DROP_ENDS_SOON = DROP_WINDOW * 5 // 6
+
+# BAD_USER_BAN_TIME = 15 * 24 * 60 * 60 # user ban time after bad behavior
+BAD_USER_BAN_TIME = 1 * 60 * 60 # user ban time after bad behavior TESTING
 
 # PATH
 CONFIG_NAME = 'config.py'  # this file's name
